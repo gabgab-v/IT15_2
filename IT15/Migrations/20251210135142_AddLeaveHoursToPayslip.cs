02f75631228ecc16c99bbbd74f754c587fe7d194
gabgab-v <g.rival.534064@umindanao.edu.ph>
2025-12-10 21:54:20 +0800

    hours migration

---
+++ IT15/Migrations/20251210135142_AddLeaveHoursToPayslip.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IT15.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLeaveHoursToPayslip : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "AbsentHours",
+                table: "PaySlips",
+                type: "numeric(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "LeaveHours",
+                table: "PaySlips",
+                type: "numeric(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AbsentHours",
+                table: "PaySlips");
+
+            migrationBuilder.DropColumn(
+                name: "LeaveHours",
+                table: "PaySlips");
+        }
+    }
+}
